7a335ad5efb2b5a2627523964922f3666d6a0e09
cernikjos <cernikjos@gmail.com>
2025-06-20 09:51:15 +0200

    test: card manager

--- src/main/java/dev/cerios/maugame/mauengine/card/CardManager.java
+++ src/main/java/dev/cerios/maugame/mauengine/card/CardManager.java
@@ -1,74 +1,154 @@
 package dev.cerios.maugame.mauengine.card;
 
+import dev.cerios.maugame.mauengine.exception.CardException;
+
 import java.util.*;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 public class CardManager {
     private final Queue<Card> deck;
     private final Queue<Card> pile;
+    private final Map<Card, Integer> floatingCards;
     private final Random random;
 
-    public static CardManager create(Random random) {
+    private final CardComparer cardComparer;
+
+    private final ReentrantReadWriteLock lock = new ReentrantReadWriteLock();
+
+    public static CardManager create(Random random, CardComparer cardComparer) {
         Set<Card> cards = new HashSet<>();
         for (CardType type : CardType.values()) {
             for (Color color : Color.values()) {
                 cards.add(new Card(type, color));
             }
         }
-        return new CardManager(cards, random)
+        return new CardManager(cards, random, cardComparer)
                 .shuffleRemaining();
     }
 
-    private CardManager(Collection<Card> cards, Random random) {
+    public CardManager(Collection<Card> cards, Random random, CardComparer cardComparer) {
+        if (cards.isEmpty())
+            throw new IllegalStateException("Cards must not be empty");
         this.deck = new LinkedList<>(cards);
         this.pile = new LinkedList<>();
+        this.floatingCards = new HashMap<>();
         this.random = random;
+        this.cardComparer = cardComparer;
     }
 
     public CardManager shuffleRemaining() {
-        List<Card> cardList = new ArrayList<>(Color.values().length * CardType.values().length);
-        while (!deck.isEmpty()) {
-            cardList.add(deck.remove());
+        try {
+            lock.writeLock().lock();
+            List<Card> cardList = new ArrayList<>(Color.values().length * CardType.values().length);
+            while (!deck.isEmpty()) {
+                cardList.add(deck.remove());
+            }
+            Collections.shuffle(cardList, random);
+            deck.addAll(cardList);
+            return this;
+        } finally {
+            lock.writeLock().unlock();
         }
-        Collections.shuffle(cardList, random);
-        deck.addAll(cardList);
-        return this;
     }
 
-    public Card draw() throws NoSuchElementException {
-        return deck.remove();
+    public Card draw() throws CardException {
+        return draw(1).getFirst();
     }
 
-    public List<Card> draw(int n) {
-        if (deck.size() < n) {
-            throw new NoSuchElementException("Cannot draw " + n + " cards, only " + deck.size() + " cards are available.");
+    public List<Card> draw(int n) throws CardException {
+        try {
+            lock.writeLock().lock();
+            if (deck.size() + pile.size() < n + 1) {
+                throw new CardException("Cannot draw " + n + " cards, only " + (deck.size() - 1) + " cards are available.");
+            }
+            List<Card> cardList = new ArrayList<>(n);
+            for (int i = 0; i < n; i++) {
+                var toDraw = deck.remove();
+                cardList.add(toDraw);
+                addFloatingCard(toDraw);
+            }
+            return cardList;
+        } finally {
+            lock.writeLock().unlock();
         }
-        List<Card> cardList = new ArrayList<>(n);
-        for (int i = 0; i < n; i++) {
-            cardList.add(deck.remove());
-        }
-        return cardList;
-    }
-
-    public void add(List<Card> cards) {
-        this.deck.addAll(cards);
     }
 
     public Card startPile() {
-        Card card = deck.remove();
-        pile.add(card);
-        return card;
+        try {
+            lock.writeLock().lock();
+            Card card = deck.remove();
+            pile.add(card);
+            return card;
+        } finally {
+            lock.writeLock().unlock();
+        }
     }
 
     public Card peekPile() {
-        return pile.peek();
+        try {
+            lock.readLock().lock();
+            return pile.peek();
+        } finally {
+            lock.readLock().unlock();
+        }
     }
 
-    public void playCard(Card card) {
-        pile.add(card);
-        deck.add(pile.remove());
+    public boolean playCard(Card card, Color nextColor) throws CardException {
+        try {
+            lock.writeLock().lock();
+            var pileCard = pile.peek();
+            if (pileCard == null)
+                throw new CardException("Pile not started.");
+
+            if (!isFloatingCard(card))
+                throw new CardException("Card does not belong to the game.");
+
+            if (!cardComparer.compare(pileCard, card))
+                return false;
+
+            if (card.type() == CardType.QUEEN) {
+                if (nextColor == null)
+                    throw new CardException("Next color not specified, when played QUEEN.");
+                cardComparer.setNextColor(nextColor);
+            } else {
+                cardComparer.clear();
+            }
+            pile.add(card);
+            deck.add(pile.remove());
+            removeFloatingCard(card);
+            return true;
+        } finally {
+            lock.writeLock().unlock();
+        }
     }
 
     public int deckSize() {
-        return deck.size();
+        try {
+            lock.readLock().lock();
+            return deck.size();
+        } finally {
+            lock.readLock().unlock();
+        }
+    }
+
+    private void addFloatingCard(Card card) {
+        floatingCards.put(card, floatingCards.getOrDefault(card, 0) + 1);
+    }
+
+    private void removeFloatingCard(Card card) {
+        int cardCount = floatingCards.getOrDefault(card, 0);
+
+        // this scenario should never happen, unless field is changed with reflection
+        if (cardCount == 0)
+            throw new IllegalStateException("Cannot remove " + card);
+
+        if (cardCount == 1)
+            floatingCards.remove(card);
+        else
+            floatingCards.put(card, cardCount - 1);
+    }
+
+    private boolean isFloatingCard(Card card) {
+        return floatingCards.containsKey(card);
     }
 }
---
+++ src/test/java/dev/cerios/maugame/mauengine/card/CardManagerTest.java
@@ -0,0 +1,366 @@
+package dev.cerios.maugame.mauengine.card;
+
+import dev.cerios.maugame.mauengine.exception.CardException;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.*;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class CardManagerTest {
+
+    private CardManager cardManager;
+    @Mock
+    private CardComparer comparer;
+
+    @BeforeEach
+    void setUp() {
+        var random = new Random(868);
+        cardManager = new CardManager(
+                List.of(
+                        new Card(CardType.QUEEN, Color.HEARTS),
+                        new Card(CardType.SEVEN, Color.SPADES),
+                        new Card(CardType.EIGHT, Color.DIAMONDS),
+                        new Card(CardType.NINE, Color.HEARTS),
+                        new Card(CardType.KING, Color.CLUBS)
+                ),
+                random,
+                comparer
+        );
+    }
+
+    @Test
+    void whenPileNotStarted_thenDeckContainsAll() {
+        // when
+        int deckSize = cardManager.deckSize();
+        Card card = cardManager.peekPile();
+
+        // then
+        assertThat(deckSize).isEqualTo(5);
+        assertThat(card).isNull();
+    }
+
+    @Test
+    void whenPileStarted_pileCardRemovedFromDeck() {
+        // when
+        Card pileCard = cardManager.startPile();
+        int deckSize = cardManager.deckSize();
+        Card peekedCard = cardManager.peekPile();
+
+        // then
+        assertThat(deckSize).isEqualTo(4);
+        assertThat(peekedCard).isSameAs(pileCard);
+        assertThat(pileCard).isEqualTo(new Card(CardType.QUEEN, Color.HEARTS));
+    }
+
+    @Test
+    void whenDrawOneCard_thenCardRemovedFromDeck() throws Exception {
+        // when
+        Card drawnCard = cardManager.draw();
+
+        // then
+        assertThat(cardManager.deckSize()).isEqualTo(4);
+        assertThat(drawnCard).isEqualTo(new Card(CardType.QUEEN, Color.HEARTS));
+        assertThat(
+                ((Queue<Card>) getField(cardManager, "deck"))
+                        .stream()
+                        .toList()
+        )
+                .containsExactly(
+                        new Card(CardType.SEVEN, Color.SPADES),
+                        new Card(CardType.EIGHT, Color.DIAMONDS),
+                        new Card(CardType.NINE, Color.HEARTS),
+                        new Card(CardType.KING, Color.CLUBS)
+                );
+    }
+
+    @Test
+    void whenDrawNCards_thenCardsRemovedFromDeck() throws Exception {
+        // when
+        var drawnCards = cardManager.draw(4);
+
+        // then
+        assertThat(cardManager.deckSize()).isEqualTo(1);
+        assertThat(drawnCards).containsExactly(
+                new Card(CardType.QUEEN, Color.HEARTS),
+                new Card(CardType.SEVEN, Color.SPADES),
+                new Card(CardType.EIGHT, Color.DIAMONDS),
+                new Card(CardType.NINE, Color.HEARTS)
+        );
+        assertThat(
+                ((Queue<Card>) getField(cardManager, "deck"))
+                        .stream()
+                        .toList()
+        )
+                .containsExactly(
+                        new Card(CardType.KING, Color.CLUBS)
+                );
+    }
+
+    @Test
+    void whenOneCardDrawnTooManyTimes_thenThrow() throws CardException {
+        // setup
+        for (int i = 0; i < 4; i++) {
+            cardManager.draw();
+        }
+
+        // when, then
+        assertThatThrownBy(() -> cardManager.draw())
+                .isInstanceOf(CardException.class);
+    }
+
+    @Test
+    void whenDrawMoreThanDeckContains_thenThrow() {
+        // when, then
+        assertThatThrownBy(() -> cardManager.draw(5))
+                .isInstanceOf(CardException.class);
+    }
+
+    @Test
+    void whenCardManagerInitializedWithEmptyCollection_thenThrow() {
+        // when, then
+        assertThatThrownBy(() -> new CardManager(Collections.emptyList(), new Random(), comparer))
+                .isInstanceOf(IllegalStateException.class);
+    }
+
+    @Test
+    void whenPlayValidCard_pileAndDeckShouldUpdate() throws Exception {
+        // setup
+        Card pileCard = cardManager.startPile();
+        Card cardToPlay = cardManager.draw();
+
+        when(comparer.compare(pileCard, cardToPlay)).thenReturn(true);
+
+        // when
+        boolean canBePlayed = cardManager.playCard(cardToPlay, null);
+
+        // then
+        assertThat(canBePlayed).isTrue();
+        assertThat(((Queue<Card>) getField(cardManager, "deck")).stream())
+                .containsExactly(
+                        new Card(CardType.EIGHT, Color.DIAMONDS),
+                        new Card(CardType.NINE, Color.HEARTS),
+                        new Card(CardType.KING, Color.CLUBS),
+                        new Card(CardType.QUEEN, Color.HEARTS)
+                );
+        assertThat(cardManager.deckSize()).isEqualTo(4);
+        assertThat(cardManager.peekPile()).isEqualTo(cardToPlay);
+        verify(comparer).clear();
+    }
+
+    @Test
+    void whenPlayInvalidCard_thenDontUpdate() throws Exception {
+        // setup
+        Card pileCard = cardManager.startPile();
+        Card cardToPlay = cardManager.draw();
+
+        when(comparer.compare(pileCard, cardToPlay)).thenReturn(false);
+
+        // when
+        boolean canBePlayed = cardManager.playCard(cardToPlay, null);
+
+        // then
+        assertThat(canBePlayed).isFalse();
+        assertThat(((Queue<Card>) getField(cardManager, "deck")).stream())
+                .containsExactly(
+                        new Card(CardType.EIGHT, Color.DIAMONDS),
+                        new Card(CardType.NINE, Color.HEARTS),
+                        new Card(CardType.KING, Color.CLUBS)
+                );
+        assertThat(cardManager.deckSize()).isEqualTo(3);
+        assertThat(cardManager.peekPile()).isEqualTo(pileCard);
+    }
+
+    @Test
+    void whenPlayNonQueenAndColorProvided_thenIgnoreIt() throws CardException {
+        // setup
+        Card pileCard = cardManager.startPile();
+        Card cardToPlay = cardManager.draw();
+
+        when(comparer.compare(pileCard, cardToPlay)).thenReturn(true);
+
+        // when
+        boolean canBePlayed = cardManager.playCard(cardToPlay, Color.SPADES);
+
+        // then
+        assertThat(canBePlayed).isTrue();
+        verify(comparer, never()).setNextColor(any(Color.class));
+        assertThat(cardManager.deckSize()).isEqualTo(4);
+        assertThat(cardManager.peekPile()).isEqualTo(cardToPlay);
+    }
+
+    @Test
+    void whenPlayQueenAndColorNotProvided_thenThrow() throws CardException {
+        // setup
+        Card cardToPlay = cardManager.draw();
+        Card pileCard = cardManager.startPile();
+
+        when(comparer.compare(pileCard, cardToPlay)).thenReturn(true);
+
+        // when, then
+        assertThatThrownBy(() -> cardManager.playCard(cardToPlay, null))
+                .isInstanceOf(CardException.class);
+    }
+
+    @Test
+    void whenPlayQueenAndColorProvided_registerIt() throws CardException {
+        // setup
+        Card cardToPlay = cardManager.draw();
+        Card pileCard = cardManager.startPile();
+
+        when(comparer.compare(pileCard, cardToPlay)).thenReturn(true);
+
+        // when
+        boolean canBePlayed = cardManager.playCard(cardToPlay, Color.SPADES);
+
+        // then
+        assertThat(canBePlayed).isTrue();
+        verify(comparer).setNextColor(any(Color.class));
+        verify(comparer, never()).clear();
+    }
+
+    @Test
+    void whenCardPlayedAndPileNotStarted_thenThrow() {
+        // when
+        assertThatThrownBy(() -> cardManager.playCard(new Card(CardType.JACK, Color.CLUBS), null))
+                .isInstanceOf(CardException.class);
+    }
+
+    @Test
+    void testShuffleRemainingCardsCorrectly() throws Exception {
+        // when
+        cardManager.shuffleRemaining();
+
+        // then
+        cardManager.shuffleRemaining();
+        assertThat(((Queue<Card>) getField(cardManager, "deck")).stream())
+                .containsExactlyInAnyOrder(
+                        new Card(CardType.QUEEN, Color.HEARTS),
+                        new Card(CardType.SEVEN, Color.SPADES),
+                        new Card(CardType.EIGHT, Color.DIAMONDS),
+                        new Card(CardType.NINE, Color.HEARTS),
+                        new Card(CardType.KING, Color.CLUBS)
+                )
+                .doesNotContainSequence(
+                        new Card(CardType.QUEEN, Color.HEARTS),
+                        new Card(CardType.SEVEN, Color.SPADES),
+                        new Card(CardType.EIGHT, Color.DIAMONDS),
+                        new Card(CardType.NINE, Color.HEARTS),
+                        new Card(CardType.KING, Color.CLUBS)
+                );
+    }
+
+    @Test
+    void testShuffleRemainingCardsWithPileCorrectly() throws Exception {
+        // setup
+        Card pileCard = cardManager.startPile();
+
+        // when
+        cardManager.shuffleRemaining();
+
+        // than
+        cardManager.shuffleRemaining();
+        assertThat(((Queue<Card>) getField(cardManager, "deck")).stream())
+                .containsExactlyInAnyOrder(
+                        new Card(CardType.SEVEN, Color.SPADES),
+                        new Card(CardType.EIGHT, Color.DIAMONDS),
+                        new Card(CardType.NINE, Color.HEARTS),
+                        new Card(CardType.KING, Color.CLUBS)
+                )
+                .doesNotContainSequence(
+                        new Card(CardType.SEVEN, Color.SPADES),
+                        new Card(CardType.EIGHT, Color.DIAMONDS),
+                        new Card(CardType.NINE, Color.HEARTS),
+                        new Card(CardType.KING, Color.CLUBS)
+                );
+        assertThat(cardManager.peekPile()).isEqualTo(pileCard);
+    }
+
+    @Test
+    void factoryIncludesFullSetOfCards() throws Exception {
+        // when
+        var created = CardManager.create(new Random(878), comparer);
+        var cardSet = new HashSet<>(((Queue<Card>) getField(created, "deck")));
+
+        // then
+        Set<Card> expected = new HashSet<>();
+        for (Color color : Color.values()) {
+            for (CardType cardType : CardType.values()) {
+                expected.add(new Card(cardType, color));
+            }
+        }
+        assertThat(cardSet).containsExactlyInAnyOrderElementsOf(expected);
+    }
+
+    @Test
+    void whenPlayCardThatDoesNotBelongToTheGame_thenThrow() {
+        // setup
+        var cardToPlay = new Card(CardType.TEN, Color.CLUBS);
+        cardManager.startPile();
+
+        // when, then
+        assertThatThrownBy(() -> cardManager.playCard(cardToPlay, null))
+                .isInstanceOf(CardException.class);
+    }
+
+    @Test
+    void testCardMultiplicationHandledCorrectly() throws Exception {
+        // setup
+        var manager = new CardManager(
+                List.of(
+                        new Card(CardType.JACK, Color.CLUBS),
+                        new Card(CardType.JACK, Color.CLUBS),
+                        new Card(CardType.JACK, Color.CLUBS)
+                ),
+                new Random(121),
+                comparer
+        );
+        manager.startPile();
+        @SuppressWarnings("unchecked")
+        var floatingCards = (Map<Card, Integer>) getField(manager, "floatingCards");
+        when(comparer.compare(any(Card.class), any(Card.class))).thenReturn(true);
+
+        // when, then
+        manager.draw();
+        assertThat(floatingCards).containsOnlyKeys(new Card(CardType.JACK, Color.CLUBS));
+        assertThat(floatingCards.get(new Card(CardType.JACK, Color.CLUBS))).isEqualTo(1);
+        manager.draw();
+        assertThat(floatingCards.get(new Card(CardType.JACK, Color.CLUBS))).isEqualTo(2);
+        manager.playCard(new Card(CardType.JACK, Color.CLUBS), null);
+        assertThat(floatingCards.get(new Card(CardType.JACK, Color.CLUBS))).isEqualTo(1);
+        manager.playCard(new Card(CardType.JACK, Color.CLUBS), null);
+        assertThat(floatingCards).isEmpty();
+        // third card is not in the game
+        assertThatThrownBy(() -> manager.playCard(new Card(CardType.JACK, Color.CLUBS), null))
+                .isInstanceOf(CardException.class);
+    }
+
+    @Test
+    void testForIllegalState_ifPrivateVarIsChangedFromOutside() throws Exception {
+        // setup
+        cardManager.startPile();
+        var card = cardManager.draw();
+        @SuppressWarnings("unchecked")
+        var floatingCards = (Map<Card,Integer>) getField(cardManager, "floatingCards");
+        when(comparer.compare(any(Card.class), any(Card.class))).thenReturn(true);
+
+        floatingCards.put(card, 0);
+
+        // when
+        assertThatThrownBy(() -> cardManager.playCard(card, null))
+                .isInstanceOf(IllegalStateException.class);
+    }
+
+    public static Object getField(Object object, String fieldName) throws Exception {
+        var field = object.getClass().getDeclaredField(fieldName);
+        field.setAccessible(true);
+        return field.get(object);
+    }
+}
