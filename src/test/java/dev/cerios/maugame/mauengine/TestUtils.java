12c151c736a40a43b0c09c0586ad4671f793ee3a
cernikjos <cernikjos@gmail.com>
2025-07-03 19:56:11 +0200

    test: extends test utils

--- src/test/java/dev/cerios/maugame/mauengine/TestUtils.java
+++ src/test/java/dev/cerios/maugame/mauengine/TestUtils.java
@@ -1,6 +1,19 @@
 package dev.cerios.maugame.mauengine;
 
+import dev.cerios.maugame.mauengine.game.GameEventListener;
+import dev.cerios.maugame.mauengine.game.Player;
+import dev.cerios.maugame.mauengine.game.action.Action;
+import lombok.Getter;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
 public class TestUtils {
+
+    public final static GameEventListener VOID_LISTENER = (p, e) -> {};
+
     public static Object getField(Object object, String fieldName) throws Exception {
         var field = object.getClass().getDeclaredField(fieldName);
         field.setAccessible(true);
@@ -11,5 +24,29 @@ public class TestUtils {
         var fieldToSet = obj.getClass().getDeclaredField(fieldName);
         fieldToSet.setAccessible(true);
         fieldToSet.set(obj, value);
+    }
+
+    public static ActionCollector createCollector() {
+        return new ActionCollector();
+    }
+
+    public static class ActionCollector {
+        private final Map<Player, List<Action>> actions = new HashMap<>();
+        @Getter
+        private final GameEventListener listener = (p, e) -> {
+            var playerActions = actions.putIfAbsent(p, new LinkedList<>());
+            if (playerActions == null) playerActions = actions.get(p);
+            playerActions.add(e);
+        };
+
+        private ActionCollector() {}
+
+        public List<Action> getActions(Player p) {
+            return actions.get(p);
+        }
+
+        public void clear() {
+            actions.clear();
+        }
     }
 }
